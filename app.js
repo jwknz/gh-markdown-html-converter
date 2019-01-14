ca0ff866431134e0eb4eb66c1ea2fe9d5f8f2836
Jeff <jeff.kranenburg>
2019-01-14 18:37:28 +1300

    option for iframe on mobile

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
     res.send('You need to add your folder path');
 })
 
-app.get('/repos/:user/:repo/content/*', (req, res) => {
+app.get('/m/repos/:user/:repo/content/*', (req, res) => {
 
 	let user = req.params.user
 	let repo = req.params.repo
@@ -47,10 +47,50 @@ app.get('/repos/:user/:repo/content/*', (req, res) => {
 			minifyJS: true
 		});
 
-		// res.write(`document.write(\`${output}\`);`);
-		// res.end();
-
 		res.send(output)
+	})
+	.catch(err => {
+		console.log(err)
+	})
+})
+
+app.get('/d/repos/:user/:repo/content/*', (req, res) => {
+
+	let user = req.params.user
+	let repo = req.params.repo
+	let pathToFile = req.params[0]
+
+	console.log(pathToFile)
+
+	let file = pathToFile.slice(0, -3);
+	file += `.md`;
+
+	console.log(file)
+	
+	let url = `https://api.github.com/repos/${user}/${repo}/contents/${file}`
+
+	request
+	.get(url)
+    .query({ 
+        client_id: process.env.CLIENT_ID, 
+        client_secret: process.env.CLIENT_SECRET
+    })
+	.set('Accept', 'application/json')
+	.then(result => {
+
+		const text = Base64.decode(result.body.content);
+
+		let converter = new showdown.Converter()
+		converter.setFlavor('github')
+		html = converter.makeHtml(text);
+
+		let output = minify(html, {
+			collapseWhitespace: true,
+			minifyJS: true
+		});
+
+		res.write(`document.write(\`${output}\`);`);
+		res.end();
 	})
 	.catch(err => {
 		console.log(err)

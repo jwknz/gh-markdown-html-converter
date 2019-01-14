de82929bebf4738be5788816e6c2810e1137a843
Jeff Kranenburg <jeffrey.kranenburg@g.toiohomai.ac.nz>
2019-01-14 14:12:29 +1300

    Added app.js and testing a path

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const showdown = require('showdown')
+const { minify } = require('html-minifier')
+const fetch = require('node-fetch')
+const Base64 = require('js-base64').Base64
+
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('You need to add your folder path');
+})
+
+app.get('/repos/:user/:repo/content/:path', (req, res) => {
+
+	let user = req.params.user
+	let repo = req.params.repo
+	let pathToFile = req.params.path
+	
+//	let url = `https://api.github.com/repos/${user}/${repo}`
+
+	console.log(user)
+	console.log(repo)
+	console.log(pathToFile)	
+	
+})

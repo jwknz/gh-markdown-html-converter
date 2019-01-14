26827f50a99b01feb509ca83ec473f4ea1420678
Jeff <jwkranenburg@icloud.com>
2019-01-14 03:11:33 +0000

    Updated routes

--- app.js
+++ app.js
@@ -6,14 +6,27 @@ const { minify } = require('html-minifier')
 const fetch = require('node-fetch')
 const Base64 = require('js-base64').Base64
 
-const port = 3000
+const port = 80
 
 
 app.get('/', (req, res) => {
     res.send('You need to add your folder path');
 })
 
-app.get('/repos/:user/:repo/content/:path', (req, res) => {
+app.get('/repos/:user/:repo/content', (req, res) => {
+
+	let user = req.params.user
+	let repo = req.params.repo
+	let pathToFile = req.params.path
+	
+//	let url = `https://api.github.com/repos/${user}/${repo}`
+
+	console.log(user)
+	console.log(repo)
+	
+})
+
+app.get('/repos/:user/:repo/content/:path*?', (req, res) => {
 
 	let user = req.params.user
 	let repo = req.params.repo
@@ -26,3 +39,5 @@ app.get('/repos/:user/:repo/content/:path', (req, res) => {
 	console.log(pathToFile)	
 	
 })
+
+app.listen(port, console.log(`Server listening....${port}`))

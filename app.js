a2d07c91ce3656a47f9ee611755bbd7f5139f5b3
Jeff <jeff.kranenburg>
2019-01-14 17:03:59 +1300

    v1.0 - Done

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
 const express = require('express')
 const app = express()
-const path = require('path')
 const showdown = require('showdown')
 const { minify } = require('html-minifier')
-const fetch = require('node-fetch')
-const Base64 = require('js-base64').Base64
+const { Base64 } = require('js-base64')
+const request = require('superagent')
+require('dotenv').load()
 
 const port = 80
-
 
 app.get('/', (req, res) => {
     res.send('You need to add your folder path');
 })
 
-app.get('/repos/:user/:repo/content', (req, res) => {
+app.get('/repos/:user/:repo/content/*', (req, res) => {
 
 	let user = req.params.user
 	let repo = req.params.repo
-	let pathToFile = req.params.path
-	
-//	let url = `https://api.github.com/repos/${user}/${repo}`
+	let pathToFile = req.params[0]
 
-	console.log(user)
-	console.log(repo)
-	
-})
+	console.log(pathToFile)
 
-app.get('/repos/:user/:repo/content/:path*?', (req, res) => {
+	let file = pathToFile.slice(0, -3);
+	file += `.md`;
 
-	let user = req.params.user
-	let repo = req.params.repo
-	let pathToFile = req.params.path
+	console.log(file)
 	
-//	let url = `https://api.github.com/repos/${user}/${repo}`
+	let url = `https://api.github.com/repos/${user}/${repo}/contents/${file}`
 
-	console.log(user)
-	console.log(repo)
-	console.log(pathToFile)	
-	
+	request
+	.get(url)
+    .query({ 
+        client_id: process.env.CLIENT_ID, 
+        client_secret: process.env.CLIENT_SECRET
+    })
+	.set('Accept', 'application/json')
+	.then(result => {
+
+		const text = Base64.decode(result.body.content);
+
+		let converter = new showdown.Converter()
+		converter.setFlavor('github')
+		html = converter.makeHtml(text);
+
+		let output = minify(html, {
+			collapseWhitespace: true,
+			minifyJS: true
+		});
+
+		res.send(output)
+	})
+	.catch(err => {
+		console.log(err)
+	})
 })
 
 app.listen(port, console.log(`Server listening....${port}`))
